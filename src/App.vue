b4deb9c80c53cf3ada35a276e63c6597732f1f32
Vilena <vylenne@Vilenas-MacBook-Pro.local>
2021-05-23 23:36:24 +0300

    test commit for repo

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <register/>
+  </div>
+</template>
+
+<script>
+
+import Register from "@/components/Register";
+export default {
+  name: 'App',
+  components: {
+    Register
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <vs-row justify="center">
+    <vs-col w="4">
+      <h4 class="notMargin">Регистрация</h4>
+      <vs-input class="mb-16" v-model="email" placeholder="Email" block>
+        <template #icon>
+          @
+        </template>
+      </vs-input>
+      <vs-input class="mb-16" type="password" v-model="password" placeholder="Пароль" block>
+        <template #icon>
+          #
+        </template>
+      </vs-input>
+      <vs-input type="password" v-model="password" placeholder="Подтвердите пароль" block>
+        <template #icon>
+          #
+        </template>
+      </vs-input>
+
+      <vs-button block class="mt-24">Зарегистрироваться</vs-button>
+    </vs-col>
+  </vs-row>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {
+    return {
+      active: true,
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.mb-16 {
+  margin-bottom: 16px;
+}
+
+.mt-24 {
+  margin-top: 24px;
+}
+</style>
